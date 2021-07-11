2dd37cba1def026aa068057a1bf4e53fff96076c
Gympie <gympie@gentoo.local>
2021-07-11 11:11:33 +0430

    random wallpaper changer added

---
+++ wall.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+WP_DIR=/home/gympie/Pictures
+#DESK=desktop
+
+BG_LIST=($WP_DIR/$DESK/*)
+N=${#BG_LIST[@]}
+((N=RANDOM%N))
+RAND_BG=$(basename ${BG_LIST[$N]})
+
+echo $RAND_BG > cur_bg.txt
+feh --bg-scale $WP_DIR/$DESK/$RAND_BG
